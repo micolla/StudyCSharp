912a2fab9c4d8f31fff3c6ebaf69572646ae778a
micolla <nikolay.dontsov@yandex.ru>
2019-02-11 20:35:58 +0300

    Добавил функцию расчета ИМТ getIMT

--- Lesson1/Lesson1/Program.cs
+++ Lesson1/Lesson1/Program.cs
@@ -13,12 +13,16 @@ namespace Lesson1
             Console.WriteLine(msg);
             return Console.ReadLine();
         }
+        static float getIMT(float height, float weight) {
+            return weight / (height*height/10000);
+        }
         static void Main(string[] args)
         {
             bool success=false;
             string firstName, lastName,agePostfix;
             int age;
-            float weight=0;
+            float weight,height=0;
+
             Console.WriteLine("Вас приветсвует программа Анкета");
             firstName = requestToUser( "Введите Ваше имя");
             lastName = requestToUser("Введите Ваше фаммилию");
@@ -27,11 +31,17 @@ namespace Lesson1
                 Console.WriteLine("Вы не верно ввели возраст, нужно ввести целое число");
                 success = int.TryParse(requestToUser("Введите Ваш полный возраст"), out age);
             }
-            success=float.TryParse(requestToUser("Введите Ваш вес"), out weight);
+            success=float.TryParse(requestToUser("Введите Ваш вес в килограммах"), out weight);
             while (!success)
             {
                 Console.WriteLine("Вы не верно ввели вес, нужно ввести целое число или дробное с запятой");
-                success = float.TryParse(requestToUser("Введите Ваш вес"), out weight);
+                success = float.TryParse(requestToUser("Введите Ваш вес в килограммах"), out weight);
+            }
+            success = float.TryParse(requestToUser("Введите Ваш рост в сантиметрах"), out height);
+            while (!success)
+            {
+                Console.WriteLine("Вы не верно ввели рост, нужно ввести целое число или дробное с запятой");
+                success = float.TryParse(requestToUser("Введите Ваш рост в сантиметрах"), out height);
             }
             if (age % 10 == 1){
                 agePostfix = "год";
@@ -40,10 +50,13 @@ namespace Lesson1
                 agePostfix = "года";
             }
             else{ agePostfix = "лет"; }
+            Console.WriteLine();
             Console.WriteLine("Вас зовут {0} \n" +
                         "Вам {1} {3} \n" +
-                        "Вы весите {2:F2} кг", firstName + " " + lastName, age, weight
-                            , agePostfix);
+                        "Вы весите {2:F2} кг\n" +
+                        "При росте {4:f2}", firstName + " " + lastName, age, weight
+                            , agePostfix,height);
+            Console.WriteLine("Ваш ИМТ равен {0:F2}", getIMT(height, weight));
             Console.ReadLine();
         }
     }
