504b202f8d8324655bd4c4e3a7bc8ea713fdd114
micolla <nikolay.dontsov@yandex.ru>
2019-02-11 20:17:50 +0300

    first exercise. Creation Request UserData, Verify and display

---
+++ Lesson1/Lesson1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson1
+{
+    class Program
+    {
+        static string requestToUser(string msg)
+        {
+            Console.WriteLine(msg);
+            return Console.ReadLine();
+        }
+        static void Main(string[] args)
+        {
+            bool success=false;
+            string firstName, lastName,agePostfix;
+            int age;
+            float weight=0;
+            Console.WriteLine("Вас приветсвует программа Анкета");
+            firstName = requestToUser( "Введите Ваше имя");
+            lastName = requestToUser("Введите Ваше фаммилию");
+            success=int.TryParse(requestToUser("Введите Ваш полный возраст"),out age );
+            while(!success) {
+                Console.WriteLine("Вы не верно ввели возраст, нужно ввести целое число");
+                success = int.TryParse(requestToUser("Введите Ваш полный возраст"), out age);
+            }
+            success=float.TryParse(requestToUser("Введите Ваш вес"), out weight);
+            while (!success)
+            {
+                Console.WriteLine("Вы не верно ввели вес, нужно ввести целое число или дробное с запятой");
+                success = float.TryParse(requestToUser("Введите Ваш вес"), out weight);
+            }
+            if (age % 10 == 1){
+                agePostfix = "год";
+            }
+            else if (age % 10>1 && age % 10 < 5) {
+                agePostfix = "года";
+            }
+            else{ agePostfix = "лет"; }
+            Console.WriteLine("Вас зовут {0} \n" +
+                        "Вам {1} {3} \n" +
+                        "Вы весите {2:F2} кг", firstName + " " + lastName, age, weight
+                            , agePostfix);
+            Console.ReadLine();
+        }
+    }
+}
