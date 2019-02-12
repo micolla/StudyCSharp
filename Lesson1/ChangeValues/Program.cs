e18f5211b0d831e9a69369c77c61b6891c249152
micolla <nikolay.dontsov@yandex.ru>
2019-02-12 21:17:31 +0300

    Add Excercise 3,4

---
+++ Lesson1/ChangeValues/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChangeValues
+{
+    class Program
+    {
+        static void changeValuesSimple(ref float x1, ref float x2)
+        {
+            float x3;
+            x3 = x1;
+            x1 = x2;
+            x2 = x3;
+        }
+        static void changeValues(ref float x1, ref float x2)
+        {
+            x1 = x2*x1;
+            x2=x1/x2;
+            x1 = x1 / x2;            
+        }
+        static void Main(string[] args)
+        {
+            float var1, var2;
+            var1 = 5;
+            var2 = 6;
+            Console.WriteLine("x1 = {0}, x2 = {1}", var1, var2);
+            changeValues(ref var1, ref var2);
+            Console.WriteLine("x1 = {0}, x2 = {1}", var1, var2);
+            var1 = 53.2f;
+            var2 = 63.54f;
+            Console.WriteLine("x1 = {0}, x2 = {1}", var1, var2);
+            changeValues(ref var1, ref var2);
+            Console.WriteLine("x1 = {0}, x2 = {1}", var1, var2);
+            Console.Read();
+        }
+    }
+}
