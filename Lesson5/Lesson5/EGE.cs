c0c1f22f7673ec81a1bc7a5d701d412cbefbdf1b
micolla <nikolay.dontsov@yandex.ru>
2019-03-06 21:08:53 +0300

    Урок 5 без 4 задачи, выполненной полностью

---
+++ Lesson5/Lesson5/EGE.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lesson5
+{
+    class EGE
+    {
+        StreamReader sr;
+        int pupilCount;
+        Dictionary<int, double> listOfStars;
+        string[,] pupilInfo;
+
+        public EGE(string filePath)
+        {
+            string tmpStr;
+            string[] tmpstrAr;
+            int i = 0;
+            Dictionary<int,Double> minDegree;
+            char[] splitChars = { ' ' };
+            if (File.Exists(filePath))
+            {
+                sr = new StreamReader(filePath);
+                if(!int.TryParse(sr.ReadLine(), out pupilCount))
+                {
+                    throw new Exception("В первой строке должно быть целое число учеников");
+                }
+                listOfStars = new Dictionary<int, double>(pupilCount);
+                pupilInfo = new string[pupilCount, 5];
+                while (!sr.EndOfStream && i<=pupilCount)
+                {
+                    tmpStr = sr.ReadLine();
+                    tmpstrAr = tmpStr.Split(splitChars);
+                    for (int j = 0; j < 5; j++)
+                    {
+                        pupilInfo[i, j] = tmpstrAr[j];
+                    }
+                    listOfStars[i] = (int.Parse(pupilInfo[i, 2]) + int.Parse(pupilInfo[i, 3])
+                        + int.Parse(pupilInfo[i, 4]))/3;
+                    i++;
+                }
+                
+            }
+            else
+                throw new FileNotFoundException($"Файла {filePath} не существует");
+        }
+    }
+}
---
+++ Lesson5/Lesson5/Task3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson5
+{
+    class Task3
+    {
+        public static bool CheckConsist(string s1,string s2)
+        {
+            char[] c1 = s1.ToCharArray();
+            char[] c2 = s2.ToCharArray();
+            Array.Sort(c1);
+            Array.Sort(c2);
+            s1 = new string(c1);
+            s2 = new string(c2);
+            return s1.CompareTo(s2) == 0 ? true : false;
+        }
+    }
+}
