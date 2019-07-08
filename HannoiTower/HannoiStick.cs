16953024d91f4e81ed83e80327209bfd043e2463
Николай Донцов <nikolay.dontsov@yandex.ru>
2019-07-08 20:45:00 +0300

    HannoiBase

---
+++ HannoiTower/HannoiStick.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HanoiTower
+{
+    class HannoiStick
+    {
+        //Stack<int> blocks;
+        List<int> blocks;
+        public int Count { get { return blocks.Count; } }
+        public HannoiStick(int blockCnt)
+        {
+            blocks = new List<int>(blockCnt);//new Stack<int>(blockCnt);
+            for (int i = 0; i < blockCnt; i++)
+            {
+                blocks.Add(i + 1);// Push(i+1);
+            }
+            
+        }
+
+        public void Push(int x)
+        {
+            blocks.Push(x);
+        }
+
+        public int Pop()
+        {
+            return blocks.Pop();
+        }
+
+        public int Show()
+        {
+            int tmp;
+            tmp = blocks.Pop();
+            //Console.WriteLine("{0}|", tmp);
+            //blocks.Pop(tmp);
+            return tmp;
+        }
+    }
+}
